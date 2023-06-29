022d9e306b936bd9e89e69d30dc1355659c363e8
wuliangjian <liangjian.wu@advantech.com.cn>
2023-06-29 22:12:29 +0800

    init

--- src/view/HomeSection.vue
+++ src/view/HomeSection.vue
@@ -2,7 +2,6 @@
   <div class="box">home</div>
   <img class="img" :src="imgUrl" />
   <img class="img" src="@/asset/images/321096.jpg" />
-  <img class="img" src="@/asset/icon/send.png" />
 </template>
 
 <script>
@@ -14,31 +13,9 @@ export default {
     };
   },
   mounted() {
-    // 获取第一个点击的坐标
   },
   methods: {
-    // 自动连点器
-    aotoClick() {
-      var pointEl = [];
-      var frequency = 500;
-      alert('请先后点击需要连点的按钮，完成后按下回车键');
-      document.onmousedown = e => {
-        pointEl.push(e);
-      };
-      document.onkeyup = e => {
-        if (e.code === 'Enter') {
-          if (!pointEl.length) return alert('你还未点击按钮');
-          setInterval(() => {
-            pointEl.forEach((event, index) => {
-              setTimeout(() => {
-                var cc = document.elementFromPoint(event.clientX, event.clientY);
-                cc.click();
-              }, frequency * index);
-            });
-          }, frequency * pointEl.length);
-        }
-      };
-    },
+ 
   },
 };
 </script>
