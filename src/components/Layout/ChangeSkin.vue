3bc4bc28609ead488982f919c930b0c3d497b8fe
wuliangjian <liangjian.wu@advantech.com.cn>
2023-07-05 19:40:26 +0800

    feat: 搭建全局背景色

--- src/components/Layout/ChangeSkin.vue
+++ src/components/Layout/ChangeSkin.vue
@@ -1,16 +1,57 @@
 <!--  -->
 <template>
   <div class="ChangeSkin">
-    <button @click="setThemeEvent('autumn')">ChangeSkin</button>
+    <div class="skin-list">
+      <div
+        v-for="skin in skinList"
+        :key="skin.value"
+        class="skin-item"
+        @click="setSkinEvent(skin.value)"
+      >
+        {{ skin.label }}
+      </div>
+    </div>
   </div>
 </template>
 
 <script setup>
-const setThemeEvent = item => {
+const skinList = [
+  {
+    label: '春天的草地',
+    value: 'spring',
+  },
+  {
+    label: '夏天的夜晚',
+    value: 'summer',
+  },
+  {
+    label: '秋天的树林',
+    value: 'autumn',
+  },
+  {
+    label: '冬天的雪花',
+    value: 'winter',
+  },
+];
+const setSkinEvent = item => {
   document.getElementsByTagName('body')[0].className = `base-style-${item}`;
 };
 </script>
 <style lang="less" scoped>
 .ChangeSkin {
+  .skin-list {
+    display: grid;
+    grid-gap: 6px;
+    grid-template-rows: auto auto;
+    grid-template-columns: auto auto;
+    .skin-item {
+      width: 100px;
+      height: 100px;
+      border: 1px solid #7c7878;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+    }
+  }
 }
 </style>
--- src/view/index.vue
+++ src/view/index.vue
@@ -1,6 +1,6 @@
 <!--  -->
 <template>
-  <section>
+  <section class="section">
     <header class="header">
       <TopBar />
     </header>
@@ -16,13 +16,14 @@ import TopBar from '@layout/TopBar.vue';
 import ChangeSkin from '@layout/ChangeSkin.vue';
 </script>
 <style lang="less" scoped>
-section {
+.section {
   display: flex;
   flex-direction: column;
   justify-content: flex-start;
   align-items: center;
   box-sizing: border-box;
   padding: 16px;
+  height: 100%;
 }
 .header {
   width: 60%;
